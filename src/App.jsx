0e0284ac9000b75b22dcd996fb1a92c1663d20cc
sabinbabu <sabinacharya444@gmail.com>
2025-02-06 12:14:22 +1100

    search feature added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,25 @@
+import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "material-icons/iconfont/material-icons.css";
+import { Container } from "react-bootstrap";
+import Header from "./components/Header";
 
 function App() {
   return (
     <>
-      <h1>Recipe Book</h1>
+      <div
+        style={{
+          backgroundImage:
+            'url("https://img.freepik.com/free-photo/food-background-with-ingredients-pasta_1220-3108.jpg")',
+          height: "100vh",
+          backgroundRepeat: "no-repeat",
+          backgroundSize: "cover",
+        }}
+      >
+        <Container fluid>
+          <Header />
+        </Container>
+      </div>
     </>
   );
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,38 @@
+import { useRef } from "react";
+import { Button, Form, Stack } from "react-bootstrap";
+import axios from "axios";
+
+const Header = () => {
+  const searchedTextRef = useRef(null);
+  const API_URL = "https://www.themealdb.com/api/json/v1/1/search.php?s=";
+
+  //   Search for recipe
+  const handleOnSearchBtn = async () => {
+    try {
+      const response = await axios.get(API_URL + searchedTextRef.current.value);
+      console.log(response.data.meals);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  return (
+    <>
+      <Stack direction="horizontal" style={{ opacity: "0.8" }} className="pt-4">
+        <h1 className="me-4">Recipe Book</h1>
+        <Form.Control
+          size="lg"
+          type="search"
+          placeholder="Search a recipe"
+          style={{ width: "60%" }}
+          className="ms-4 rounded-pill"
+          ref={searchedTextRef}
+        />
+        <Button variant="transparent" onClick={handleOnSearchBtn}>
+          <span className="material-icons">search</span>
+        </Button>
+      </Stack>
+    </>
+  );
+};
+
+export default Header;
